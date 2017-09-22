f2db1dc899f818d580666a17128472174295e831
François Kooman <fkooman@tuxed.net>
2017-09-22 21:19:21 +0200

    fix autoloader finder

--- bin/generate.php
+++ bin/generate.php
@@ -15,18 +15,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-$autoloadFiles = [
-    sprintf('%s/src/autoload.php', dirname(__DIR__)),
-    sprintf('%s/vendor/autoload.php', dirname(__DIR__)),
-];
+$baseDir = dirname(__DIR__);
 
-foreach ($autoloadFiles as $autoloadFile) {
-    if (file_exists($autoloadFile)) {
-        require_once $autoloadFile;
+// find the autoloader (package installs, composer)
+foreach (['src', 'vendor'] as $autoloadDir) {
+    if (@file_exists(sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir))) {
+        require_once sprintf('%s/%s/autoload.php', $baseDir, $autoloadDir);
         break;
     }
 }
-
 use fkooman\SAML\DS\Config;
 use fkooman\SAML\DS\HttpClient\CurlHttpClient;
 use fkooman\SAML\DS\Logo;
