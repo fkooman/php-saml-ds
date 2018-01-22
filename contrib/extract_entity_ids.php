6ba46c70273bde3e7203e14298a2e4764743dadc
François Kooman <fkooman@tuxed.net>
2018-01-22 18:45:48 +0100

    update (C) header

--- contrib/extract_entity_ids.php
+++ contrib/extract_entity_ids.php
@@ -1,12 +1,13 @@
 <?php
-/**
- * Copyright 2017 François Kooman <fkooman@tuxed.net>.
+
+/*
+ * Copyright 2017,2018  François Kooman <fkooman@tuxed.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 try {
     if (false === $md = @simplexml_load_file($argv[1])) {
         throw new RuntimeException(sprintf('unable to read file "%s"', $argv[1]));
